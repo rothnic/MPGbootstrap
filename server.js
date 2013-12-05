2cacefd323a295097b8a95e9aca195673fefab52
rothnic <nlr06886@gmail.com>
2013-12-05 09:13:39 -0600

    Fixed server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 
 var connect = require('connect'),
    http = require('http');
-var port = proc.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 
 connect()
    .use(connect.static(__dirname))
