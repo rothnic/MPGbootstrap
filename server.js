21ac8b30b71a60837feb3d13deabd99bc9fc89b3
rothnic <nlr06886@gmail.com>
2013-12-05 04:23:11 -0600

    Fixed server.js.

--- server.js
+++ server.js
@@ -5,6 +5,6 @@
 var connect = require('connect'),
    http = require('http');
 connect()
-   .use(connect.static(__dirname + "/web/mpg/"))
+   .use(connect.static(__dirname))
    .use(connect.directory(__dirname))
-   .listen(process.env.PORT);
+   .listen(5000);
