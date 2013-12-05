02fe9889978014c2ac79e50c681463cae935916c
rothnic <nlr06886@gmail.com>
2013-12-05 16:44:22 -0600

    Fixed data formatting issue that was causing strange behavior on selecting a single drive.

--- web/mpg/js/mpg/mpgcharts.js
+++ web/mpg/js/mpg/mpgcharts.js
@@ -271,7 +271,7 @@ define(["dc","d3", "d3v2", "jquery","crossfilter","colorbrewer","d3Tooltip", "js
 
             yearlyBubbleChart
                 .width(450) // (optional) define chart width, :default = 200
-                .height(320)  // (optional) define chart height, :default = 200
+                .height(316)  // (optional) define chart height, :default = 200
                 .transitionDuration(1500) // (optional) define chart transition duration, :default = 750
                 .margins({top: 10, right: 80, bottom: 50, left: 40})
                 .dimension(yearlyDimension)
@@ -680,8 +680,7 @@ define(["dc","d3", "d3v2", "jquery","crossfilter","colorbrewer","d3Tooltip", "js
                             //var now = (new Date()).getTime() - 86400 * 1000 * 365;
                             //now = new Date(now + d * 86400 * 1000);
                             //return d3.time.format('%b %d %Y')(now);
-                            return d3.time.format('%b %e %H:%M:%S %Y')(new Date(d));
-                        }
+                            return d3.time.format('%b %e %H:%M:%S %Y')(new Date(d));}
                     }
                 else {
                     formatter = d3.format(",.1f");
@@ -690,11 +689,11 @@ define(["dc","d3", "d3v2", "jquery","crossfilter","colorbrewer","d3Tooltip", "js
                     for (var k=0; k<inputData.length - 1; k++){
                         var thisExtent = d3.extent(inputData[k].values.map(function(d){return d.x;}));
                         dateArray.push(thisExtent[0]);
-                        dateArray.push(thisExtent[1]);
-                    }
+                        dateArray.push(thisExtent[1]);}
                     var theDomain = d3.time.scale().domain(d3.extent(dateArray));
                     mpgcharts.seriesChart.xAxis = d3.time.scale().domain(d3.extent(theDomain));
                     mpgcharts.seriesChart.x2Axis = d3.time.scale().domain(d3.extent(theDomain));*/
+
                 mpgcharts.seriesChart.xAxis
                     .tickFormat(d3.format(',.2f'));
 
@@ -706,12 +705,10 @@ define(["dc","d3", "d3v2", "jquery","crossfilter","colorbrewer","d3Tooltip", "js
                     .tickFormat(d3.format(',.2f'));
 
                  // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
-
                 mpgcharts.seriesChart
                     .y2Axis.axisLabel('Value')
                     .tickFormat(d3.format(',.2f'));
                 //mpgcharts.seriesChart.forceX(d3.extent(theDomain));
-
                 d3.select('#' + containerid + ' svg')
                     .datum(inputData)
                   .transition().duration(1000)
@@ -721,6 +718,7 @@ define(["dc","d3", "d3v2", "jquery","crossfilter","colorbrewer","d3Tooltip", "js
                 return mpgcharts.seriesChart;
               });
             }
+
 
             // This function collects the grouped driveID data into the format needed to plot in a line graph
             function seriesData(data, thekeys){
@@ -898,6 +896,146 @@ define(["dc","d3", "d3v2", "jquery","crossfilter","colorbrewer","d3Tooltip", "js
           for(m = r.mean = s / t, l = t, s = 0; l--; s += Math.pow(a[l] - m, 2));
           return r.deviation = Math.sqrt(r.variance = s / t), r;
         }
+
+        function scatterPlot(inputData){
+
+            var width = 960,
+                size = 150,
+                padding = 19.5;
+
+            var x = d3.scale.linear()
+                .range([padding / 2, size - padding / 2]);
+
+            var y = d3.scale.linear()
+                .range([size - padding / 2, padding / 2]);
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom")
+                .ticks(5);
+
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient("left")
+                .ticks(5);
+
+            var color = d3.scale.category10();
+
+            d3.csv("flowers.csv", function(error, data) {
+              var domainByTrait = {},
+                  traits = d3.keys(data[0]).filter(function(d) { return d !== "species"; }),
+                  n = traits.length;
+
+              traits.forEach(function(trait) {
+                domainByTrait[trait] = d3.extent(data, function(d) { return d[trait]; });
+              });
+
+              xAxis.tickSize(size * n);
+              yAxis.tickSize(-size * n);
+
+              var brush = d3.svg.brush()
+                  .x(x)
+                  .y(y)
+                  .on("brushstart", brushstart)
+                  .on("brush", brushmove)
+                  .on("brushend", brushend);
+
+              var svg = d3.select("body").append("svg")
+                  .attr("width", size * n + padding)
+                  .attr("height", size * n + padding)
+                .append("g")
+                  .attr("transform", "translate(" + padding + "," + padding / 2 + ")");
+
+              svg.selectAll(".x.axis")
+                  .data(traits)
+                .enter().append("g")
+                  .attr("class", "x axis")
+                  .attr("transform", function(d, i) { return "translate(" + (n - i - 1) * size + ",0)"; })
+                  .each(function(d) { x.domain(domainByTrait[d]); d3.select(this).call(xAxis); });
+
+              svg.selectAll(".y.axis")
+                  .data(traits)
+                .enter().append("g")
+                  .attr("class", "y axis")
+                  .attr("transform", function(d, i) { return "translate(0," + i * size + ")"; })
+                  .each(function(d) { y.domain(domainByTrait[d]); d3.select(this).call(yAxis); });
+
+              var cell = svg.selectAll(".cell")
+                  .data(cross(traits, traits))
+                .enter().append("g")
+                  .attr("class", "cell")
+                  .attr("transform", function(d) { return "translate(" + (n - d.i - 1) * size + "," + d.j * size + ")"; })
+                  .each(plot);
+
+              // Titles for the diagonal.
+              cell.filter(function(d) { return d.i === d.j; }).append("text")
+                  .attr("x", padding)
+                  .attr("y", padding)
+                  .attr("dy", ".71em")
+                  .text(function(d) { return d.x; });
+
+              cell.call(brush);
+
+              function plot(p) {
+                var cell = d3.select(this);
+
+                x.domain(domainByTrait[p.x]);
+                y.domain(domainByTrait[p.y]);
+
+                cell.append("rect")
+                    .attr("class", "frame")
+                    .attr("x", padding / 2)
+                    .attr("y", padding / 2)
+                    .attr("width", size - padding)
+                    .attr("height", size - padding);
+
+                cell.selectAll("circle")
+                    .data(data)
+                  .enter().append("circle")
+                    .attr("cx", function(d) { return x(d[p.x]); })
+                    .attr("cy", function(d) { return y(d[p.y]); })
+                    .attr("r", 3)
+                    .style("fill", function(d) { return color(d.species); });
+              }
+
+              var brushCell;
+
+              // Clear the previously-active brush, if any.
+              function brushstart(p) {
+                if (brushCell !== this) {
+                  d3.select(brushCell).call(brush.clear());
+                  x.domain(domainByTrait[p.x]);
+                  y.domain(domainByTrait[p.y]);
+                  brushCell = this;
+                }
+              }
+
+              // Highlight the selected circles.
+              function brushmove(p) {
+                var e = brush.extent();
+                svg.selectAll("circle").classed("hidden", function(d) {
+                  return e[0][0] > d[p.x] || d[p.x] > e[1][0]
+                      || e[0][1] > d[p.y] || d[p.y] > e[1][1];
+                });
+              }
+
+              // If the brush is empty, select all circles.
+              function brushend() {
+                if (brush.empty()) svg.selectAll(".hidden").classed("hidden", false);
+              }
+
+              function cross(a, b) {
+                var c = [], n = a.length, m = b.length, i, j;
+                for (i = -1; ++i < n;) for (j = -1; ++j < m;) c.push({x: a[i], i: i, y: b[j], j: j});
+                return c;
+              }
+
+              d3.select(self.frameElement).style("height", size * n + padding + 20 + "px");
+            });
+
+
+        }
+
 
         //#### Version
         //Determine the current version of dc with `dc.version`
